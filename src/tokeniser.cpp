b96507ac10bf08136a6520f7e2b4418dc7a09853
Elias Lehtinen <elias.lehtinen01@gmail.com>
2024-01-19 17:20:46 +0200

    Use parse function for different types of tokens

--- src/tokeniser.cpp
+++ src/tokeniser.cpp
@@ -17,56 +17,30 @@ namespace tokeniser {
     return content;
   }
 
-  void literals(const std::string& content,
-      std::back_insert_iterator<std::vector<Token>> inserter) {
-    std::regex regex("\\d");
+  void parse(const std::string& content,
+      std::back_insert_iterator<std::vector<Token>> inserter,
+      std::vector<std::regex>::const_iterator regex_begin,
+      std::vector<std::regex>::const_iterator regex_end,
+      std::vector<Type>::const_iterator types_begin,
+      std::vector<Type>::const_iterator types_end) {
+    if (regex_begin == regex_end) return;
+
     auto lines_begin =
-      std::sregex_iterator(content.begin(), content.end(), regex);
-    auto lines_end = std::sregex_iterator();
-
-    for (std::sregex_iterator i = lines_begin; i != lines_end; i++) {
-      inserter = Token(Type::literal, i->str());
-    }
-  }
-
-  void identifiers(const std::string& content,
-      std::back_insert_iterator<std::vector<Token>> inserter) {
-    std::vector<Token> tokens;
-
-    std::regex regex("[+-]");
-    auto lines_begin =
-      std::sregex_iterator(content.begin(), content.end(), regex);
+      std::sregex_iterator(content.begin(), content.end(), *regex_begin);
     auto lines_end = std::sregex_iterator();
 
     int last = 0;
     for (std::sregex_iterator i = lines_begin; i != lines_end; i++) {
       int pos = i->position();
-      std::string before = content.substr(last, pos-last);
-      literals(before, inserter);
-      inserter = Token(Type::identifier, i->str());
+      std::string between = content.substr(last, pos-last);
+      parse(between, inserter, regex_begin+1, regex_end,
+          types_begin+1, types_end);
+      inserter = Token(*types_begin, i->str());
       last = pos+1;
     }
-
-    std::string before = content.substr(last, content.size());
-    literals(before, inserter);
-  }
-
-  void lines(const std::string& content,
-      std::back_insert_iterator<std::vector<Token>> inserter) {
-    std::regex regex("\\n");
-    auto lines_begin =
-      std::sregex_iterator(content.begin(), content.end(), regex);
-    auto lines_end = std::sregex_iterator();
-
-    int last = 0;
-    for (std::sregex_iterator i = lines_begin; i != lines_end; i++) {
-      int pos = i->position();
-      std::string line = content.substr(last, pos-last);
-      identifiers(line, inserter);
-      inserter = Token(Type::eol, "\n");
-      last = pos+1;
-    }
-    inserter = Token(Type::end, "END");
+    std::string between = content.substr(last, content.size());
+    parse(between, inserter, regex_begin+1, regex_end,
+        types_begin+1, types_end);
   }
 
   void tokenise(const std::string& filename) {
@@ -76,8 +50,24 @@ namespace tokeniser {
     std::cout << "tokenise: " << filename << std::endl;
     std::string content = read(filename + input_extension);
 
+    std::vector<std::regex> expressions = {
+      std::regex("\\n"),
+      std::regex("[+-]"),
+      std::regex("\\d"),
+    };
+    std::vector<Type> types = {
+      Type::eol,
+      Type::identifier,
+      Type::literal,
+    };
+
     std::vector<Token> tokens;
-    lines(content, std::back_inserter(tokens));
+    parse(content, std::back_inserter(tokens),
+        expressions.begin(),
+        expressions.end(),
+        types.begin(),
+        types.end());
+    tokens.push_back(Token(Type::end, "END"));
 
     std::string result = "";
     for (const Token& token: tokens) {
