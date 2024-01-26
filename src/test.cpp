d145689b0d69592b7ca2b1f4e99b597a7d64a708
Elias Lehtinen <elias.lehtinen01@gmail.com>
2024-01-26 10:59:46 +0200

    Reformat test output

--- src/test.cpp
+++ src/test.cpp
@@ -5,42 +5,50 @@
 #include "readwrite.h"
 #include "tokeniser.h"
 
-
-void test_file(const std::string& file) {
+std::string input_file(const std::string& file) {
   const std::string input_extension = ".alpha";
-  const std::string test_extension = ".tokens.test";
+  return file + input_extension;
+}
 
-  const std::string input_file = file + input_extension;
-  const std::string test_file = file + test_extension;
+std::string output_file(const std::string& file) {
+  const std::string output_extension = ".tokens";
+  return file + output_extension;
+}
 
-  if (!is_file(file + input_extension)) {
-    std::cout << "File " << input_file << " doesn't exists" << std::endl;
-    return;
-  }
-
-  std::string content = read(input_file);
-  std::vector<tokeniser::Token> tokens = tokeniser::tokenise(content);
-  std::string result = to_string(tokens);
-
+void check_result(const std::string& file, const std::string& input, const std::string& output) {
   bool write_to_file = true;
-  if (is_file(test_file)) {
-    std::string test_content = read(test_file);
-    if (test_content == result) {
-      std::cout << "File " << file << " passed" << std::endl;
+  if (is_file(output_file(file))) {
+    std::string correct = read(output_file(file));
+    if (correct == output) {
+      std::cout << "File passed: " << file << std::endl;
       return;
     }
-    std::cout << "File " << file << " failed" << std::endl;
+
+    std::cout << "File failed: " << file << std::endl;
     std::cout << "Input:" << std::endl;
-    std::cout << content << std::endl;
+    std::cout << input << std::endl;
     std::cout << "Output:" << std::endl;
-    std::cout << result << std::endl;
+    std::cout << output << std::endl;
     std::cout << "Correct:" << std::endl;
-    std::cout << test_content << std::endl;
+    std::cout << correct << std::endl;
     write_to_file = user_approval("Do you want to override [Y/n]? ");
   }
 
   if (!write_to_file) return;
 
-  write(test_file, result);
-  std::cout << "File " << file << " saved" << std::endl;
+  write(output_file(file), output);
+  std::cout << "File saved: " << file << std::endl;
+}
+
+void test_file(const std::string& file) {
+  if (!is_file(input_file(file))) {
+    std::cout << "File doesn't exists: " << input_file(file) << std::endl;
+    return;
+  }
+
+  std::string input = read(input_file(file));
+  std::vector<tokeniser::Token> tokens = tokeniser::tokenise(input);
+  std::string output = to_string(tokens);
+
+  check_result(file, input, output);
 }
