1eaeb27cbf5250de09e85db3c0b8ab2e2c72792f
Elias Lehtinen <elias.lehtinen01@gmail.com>
2024-02-02 12:55:58 +0200

    Generate interpretation file

--- src/test.cpp
+++ src/test.cpp
@@ -10,19 +10,20 @@
 #include "compiler.h"
 
 
-std::string alpha_file(const std::string& file) {
-  const std::string input_extension = ".alpha";
-  return file + input_extension;
-}
+enum class FileType {
+  source,
+  tokens,
+  tree,
+  interpret,
+};
 
-std::string token_file(const std::string& file) {
-  const std::string output_extension = ".tokens";
-  return file + output_extension;
-}
-
-std::string tree_file(const std::string& file) {
-  const std::string output_extension = ".tree";
-  return file + output_extension;
+std::string filename(const std::string& file, const FileType type) {
+  std::string suffix = ".unknown";
+  if (type == FileType::source) suffix = ".alpha";
+  if (type == FileType::tokens) suffix = ".tokens";
+  if (type == FileType::tree) suffix = ".tree";
+  if (type == FileType::interpret) suffix = ".interpret";
+  return file + suffix;
 }
 
 void check_result(const std::string& in_file, const std::string& out_file,
@@ -52,16 +53,19 @@ void check_result(const std::string& in_file, const std::string& out_file,
 }
 
 void test_file(const std::string& file) {
-  if (!is_file(alpha_file(file))) {
-    std::cout << "File doesn't exists: " << alpha_file(file) << std::endl;
+  if (!is_file(filename(file, FileType::source))) {
+    std::cout << "File doesn't exists: ";
+    std::cout << filename(file, FileType::source) << std::endl;
     return;
   }
 
-  std::string source = read(alpha_file(file));
+  std::string source = read(filename(file, FileType::source));
   compiler::Compiler compiler(source);
 
-  check_result(alpha_file(file), token_file(file),
+  check_result(filename(file, FileType::source), filename(file, FileType::tokens),
       source, compiler.tokens(false));
-  check_result(token_file(file), tree_file(file),
+  check_result(filename(file, FileType::tokens), filename(file, FileType::tree),
       compiler.tokens(false), compiler.tree(false));
+  check_result(filename(file, FileType::tree), filename(file, FileType::interpret),
+      compiler.tree(false), compiler.interpret(false));
 }
